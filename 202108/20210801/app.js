c52453e73a92a962d56169d6e1abfff4bba3f81e
k_tsuda <h240503@gmail.com>
2021-08-01 20:49:14 +0900

    feat: make title class

--- 202108/20210801/app.js
+++ 202108/20210801/app.js
@@ -1,10 +1,32 @@
-// importでエラーになる
+// 参考：
+// https://qiita.com/TsutomuNakamura/items/72d8cf9f07a5a30be048
+// ↓CDNだとimportでエラーになる？
 // import Layout from "https://raw.githubusercontent.com/k-tsuda400/JavaScriptTraining/master/202108/20210801/components/Layout.js";
+// 仕方ないので、分離せず、1ファイルにクラスを書いていく
 
+// ------------------------------------------------------
+class Title extends React.Component {
+  render() {
+    return (
+      <h1>Welcome!</h1>
+    );
+  }
+}
+
+// ------------------------------------------------------
 class Header extends React.Component {
   render() {
     return (
       <header>header</header>
+    );
+  }
+}
+
+// ------------------------------------------------------
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>footer</footer>
     );
   }
 }
@@ -22,7 +44,9 @@ class Layout extends React.Component {
     //  <h1>It's { this.state.text }!</h1>
     //);
     return (
-      <Header />
+      <header>
+        <Title />
+      </header>
     );
   }
 }
