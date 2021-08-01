73bb956c97bc3f6d35048d18d87c0e9a3c52f3b9
k_tsuda <h240503@gmail.com>
2021-08-01 11:15:57 +0900

    refactor components

--- 202108/20210801/app.js
+++ 202108/20210801/app.js
@@ -1,36 +1,4 @@
-class Title extends React.Component {
-  render() {
-    return (
-      <h1>Welcome!</h1>
-    );
-  }
-}
+import Layout from "https://raw.githubusercontent.com/k-tsuda400/JavaScriptTraining/master/202108/20210801/components/Layout.js";
 
-class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        text: 'Hello World!'
-      } 
-    }
-
-    handleChange(e) {
-      const title = e.target.value;
-      this.props.changeTitle(title);
-    }
-    
-    render() {
-      return (
-        <div>
-          <p> { this.state.text } </p>
-          <Title title={this.props.title} />
-          <input value={this.props.title} onChange={this.handleChange.bind(this)} />
-        </div>
-      )
-    }
-  }
-      
-  ReactDOM.render(
-    <App />,
-    document.getElementById('app') //htmlの<div id="app"></div>をターゲットに設定している。
-  );
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
