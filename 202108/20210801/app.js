1a8f0bdab3ff151593c078c3b8e9369df840a1ba
k_tsuda <h240503@gmail.com>
2021-08-01 20:31:26 +0900

    fix components化 (no split file)

--- 202108/20210801/app.js
+++ 202108/20210801/app.js
@@ -1,4 +1,17 @@
-import Layout from "https://raw.githubusercontent.com/k-tsuda400/JavaScriptTraining/master/202108/20210801/components/Layout.js";
+// importでエラーになる
+// import Layout from "https://raw.githubusercontent.com/k-tsuda400/JavaScriptTraining/master/202108/20210801/components/Layout.js";
+
+class Layout extends React.Component {
+  constructor() {
+    super();
+    this.name = "Test";
+  }
+  render() {
+    return (
+      <h1>It's {this.name}!</h1>
+    );
+  }
+}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
