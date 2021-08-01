d005cc4f9f2129f81131709fb66fb22614d0997a
k_tsuda <h240503@gmail.com>
2021-08-01 11:04:46 +0900

    app js upload

---
+++ 202108/20210801/app.js
@@ -0,0 +1,28 @@
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        text: 'Hello World!'
+      } 
+    }
+
+    handleChange(e) {
+      const title = e.target.value;
+      this.props.changeTitle(title);
+    }
+    
+    render() {
+      return (
+        <div>
+          <p> { this.state.text } </p>
+          <Title title={this.props.title} />
+          <input value={this.props.title} onChange={this.handleChange.bind(this)} />
+        </div>
+      )
+    }
+  }
+      
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app') //htmlの<div id="app"></div>をターゲットに設定している。
+  );
