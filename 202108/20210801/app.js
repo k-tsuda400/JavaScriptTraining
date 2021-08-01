ce4369129723ca9ec02ba1a30158aa4f113bded5
k_tsuda <h240503@gmail.com>
2021-08-01 20:38:27 +0900

    fix using 0611 code

--- 202108/20210801/app.js
+++ 202108/20210801/app.js
@@ -2,16 +2,20 @@
 // import Layout from "https://raw.githubusercontent.com/k-tsuda400/JavaScriptTraining/master/202108/20210801/components/Layout.js";
 
 class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "Test";
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello World!'
+    } 
   }
   render() {
     return (
-      <h1>It's {this.name}!</h1>
+      <h1>It's { this.state.text }!</h1>
     );
   }
 }
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+  <Layout />,
+  document.getElementById('app') //htmlの<div id="app"></div>をターゲットに設定している。
+);
